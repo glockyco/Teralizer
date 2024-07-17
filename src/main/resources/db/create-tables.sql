3ccae133a224fd5b0a956e1b940a0b0352b46951
Johann Glock <johann.glock@aau.at>
2024-07-17 13:25:26 +0200

    Reformat create-tables.sql

--- src/main/resources/db/create-tables.sql
+++ src/main/resources/db/create-tables.sql
@@ -10,41 +10,41 @@ CREATE TABLE project
     id        INTEGER PRIMARY KEY AUTOINCREMENT,
     path      TEXT NOT NULL,
     classpath TEXT, -- can be null for invalid project paths
-    runtime   REAL -- can be null for projects with failed project-level tasks
+    runtime   REAL  -- can be null for projects with failed project-level tasks
 );
 
 CREATE TABLE test
 (
     id                        INTEGER PRIMARY KEY AUTOINCREMENT,
     project_id                INTEGER NOT NULL,
-    test_class_path           TEXT NOT NULL,
-    test_class_package        TEXT NOT NULL,
-    test_class_name           TEXT NOT NULL,
-    test_method_name          TEXT NOT NULL,
-    tested_class_path         TEXT NOT NULL,
-    tested_class_package      TEXT NOT NULL,
-    tested_class_name         TEXT NOT NULL,
-    tested_method_name        TEXT NOT NULL,
-    tested_method_param_types TEXT NOT NULL,
-    tested_method_return_type TEXT NOT NULL,
-    driver_class_path         TEXT NOT NULL,
-    driver_class_package      TEXT NOT NULL,
-    driver_class_name         TEXT NOT NULL,
-    jpf_config_path           TEXT NOT NULL,
-    input_specification_path  TEXT NOT NULL,
-    output_specification_path TEXT NOT NULL,
-    FOREIGN KEY (project_id) REFERENCES project(id) ON DELETE CASCADE
+    test_class_path           TEXT    NOT NULL,
+    test_class_package        TEXT    NOT NULL,
+    test_class_name           TEXT    NOT NULL,
+    test_method_name          TEXT    NOT NULL,
+    tested_class_path         TEXT    NOT NULL,
+    tested_class_package      TEXT    NOT NULL,
+    tested_class_name         TEXT    NOT NULL,
+    tested_method_name        TEXT    NOT NULL,
+    tested_method_param_types TEXT    NOT NULL,
+    tested_method_return_type TEXT    NOT NULL,
+    driver_class_path         TEXT    NOT NULL,
+    driver_class_package      TEXT    NOT NULL,
+    driver_class_name         TEXT    NOT NULL,
+    jpf_config_path           TEXT    NOT NULL,
+    input_specification_path  TEXT    NOT NULL,
+    output_specification_path TEXT    NOT NULL,
+    FOREIGN KEY (project_id) REFERENCES project (id) ON DELETE CASCADE
 );
 
 CREATE TABLE generalization
 (
     id                        INTEGER PRIMARY KEY AUTOINCREMENT,
     test_id                   INTEGER NOT NULL,
-    tool                      TEXT NOT NULL,
-    generalized_class_path    TEXT NOT NULL,
-    generalized_class_package TEXT NOT NULL,
-    generalized_class_name    TEXT NOT NULL,
-    FOREIGN KEY (test_id) REFERENCES test(id) ON DELETE CASCADE
+    tool                      TEXT    NOT NULL,
+    generalized_class_path    TEXT    NOT NULL,
+    generalized_class_package TEXT    NOT NULL,
+    generalized_class_name    TEXT    NOT NULL,
+    FOREIGN KEY (test_id) REFERENCES test (id) ON DELETE CASCADE
 );
 
 CREATE TABLE task
@@ -54,11 +54,11 @@ CREATE TABLE task
     test_id           INTEGER, -- can be null for project-level tasks
     generalization_id INTEGER, -- can be null for project-/test-level tasks
     step              INTEGER NOT NULL,
-    stage             TEXT NOT NULL,
-    status            TEXT NOT NULL,
-    runtime           REAL, -- can be null for failed tasks
-    error             TEXT, -- can be null for successful tasks
-    FOREIGN KEY (project_id) REFERENCES project(id) ON DELETE CASCADE,
-    FOREIGN KEY (test_id) REFERENCES  test(id) ON DELETE CASCADE,
-    FOREIGN KEY (generalization_id) REFERENCES generalization(id) ON DELETE CASCADE
+    stage             TEXT    NOT NULL,
+    status            TEXT    NOT NULL,
+    runtime           REAL,    -- can be null for failed tasks
+    error             TEXT,    -- can be null for successful tasks
+    FOREIGN KEY (project_id) REFERENCES project (id) ON DELETE CASCADE,
+    FOREIGN KEY (test_id) REFERENCES test (id) ON DELETE CASCADE,
+    FOREIGN KEY (generalization_id) REFERENCES generalization (id) ON DELETE CASCADE
 );
