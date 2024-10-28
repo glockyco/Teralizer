00acac323f1e99e1b53bd3290e1807c9b0fd4ce6
Johann Glock <johann.glock@aau.at>
2024-10-28 11:42:56 +0100

    Add missing values to MutationStatus enum
    The MutationStatus class needs to support all (non-internal-only)
    status values that PIT can report.

--- src/main/java/teralizer/processing/MutationStatus.java
+++ src/main/java/teralizer/processing/MutationStatus.java
@@ -1,6 +1,13 @@
 package teralizer.processing;
 
 public enum MutationStatus {
-    SURVIVED,
+    // MutationStatus values represent the corresponding values of PIT's DetectionStatus class:
+    // https://github.com/hcoles/pitest/blob/d23455e34dd6660d7df617fdeb55ab31b636870d/pitest/src/main/java/org/pitest/mutationtest/DetectionStatus.java
     KILLED,
+    SURVIVED,
+    TIMED_OUT,
+    NON_VIABLE,
+    MEMORY_ERROR,
+    RUN_ERROR,
+    NO_COVERAGE,
 }
