ccae20142d07a9c458bb4a099863d057d42f56e9
Johann Glock <johann.glock@aau.at>
2024-11-26 14:17:20 +0100

    Add methods with equality checks to example projects

--- projects/example-gradle-junit4/src/main/java/teralizer/example/MyMath.java
+++ projects/example-gradle-junit4/src/main/java/teralizer/example/MyMath.java
@@ -8,4 +8,12 @@ public class MyMath {
     public int abs(int x) {
         return x >= 0 ? x : -x;
     }
+
+    public boolean isZero(int x) {
+        return x == 0;
+    }
+
+    public boolean isEqual(int x, int y) {
+        return x == y;
+    }
 }
--- projects/example-gradle-junit4/src/test/java/teralizer/example/MyMathTest.java
+++ projects/example-gradle-junit4/src/test/java/teralizer/example/MyMathTest.java
@@ -15,4 +15,23 @@ public class MyMathTest {
         int actual = math.add(x, y);
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void testIsZero() {
+        int x = 0;
+        boolean expected = true;
+        MyMath math = new MyMath();
+        boolean actual = math.isZero(x);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsEqual() {
+        int x = 0;
+        int y = 0;
+        boolean expected = true;
+        MyMath math = new MyMath();
+        boolean actual = math.isEqual(x, y);
+        assertEquals(expected, actual);
+    }
 }
