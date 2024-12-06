d476dbc255cf1b5dd86c7f92e1080faf9e8b0427
Johann Glock <johann.glock@aau.at>
2024-12-06 15:43:13 +0100

    Add name of test method to generalized test
    Having the qualified name of the corresponding test method easily
    available (rather than having to go through the DB to find it or
    manually piecing it together from other information in the
    generalized test) makes it much easier to quickly navigate to it.

--- src/main/java/teralizer/processing/task/TestGeneralizationTask.java
+++ src/main/java/teralizer/processing/task/TestGeneralizationTask.java
@@ -147,6 +147,7 @@ public class TestGeneralizationTask extends AbstractTask {
         }
 
         generalizedClassDeclaration.getElements(e -> true).forEach(e -> e.getComments().clear());
+        generalizedClassDeclaration.addComment(factory.createInlineComment("Test: " + this.testRecord.getTestMethodQualifiedName()));
         generalizedClassDeclaration.addComment(factory.createInlineComment("Input specification: " + this.testRecord.getInputSpecificationPath()));
         generalizedClassDeclaration.addComment(factory.createInlineComment("Output specification: " + this.testRecord.getOutputSpecificationPath()));
 
