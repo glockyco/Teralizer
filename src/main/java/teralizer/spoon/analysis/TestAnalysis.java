7869db251302ef3fdd9c9f81d09f977d7d91899d
Johann Glock <johann.glock@aau.at>
2024-12-14 10:15:17 +0100

    Improve robustness of assertion detection

--- src/main/java/teralizer/spoon/analysis/TestAnalysis.java
+++ src/main/java/teralizer/spoon/analysis/TestAnalysis.java
@@ -2,6 +2,8 @@ package teralizer.spoon.analysis;
 
 import spoon.reflect.code.CtInvocation;
 import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.reference.CtExecutableReference;
+import spoon.reflect.reference.CtTypeReference;
 import spoon.reflect.visitor.filter.TypeFilter;
 
 import java.util.List;
@@ -9,12 +11,15 @@ import java.util.stream.Collectors;
 
 public class TestAnalysis {
 
-    public static CtInvocation<?> findTestedMethodCall(CtMethod<?> testMethodDeclaration) {
+    private static final String JUNIT4_ASSERTION_PACKAGE = "org.junit.Assert";
+    private static final String JUNIT5_ASSERTION_PACKAGE = "org.junit.jupiter.api.Assertions";
+
+    public static CtInvocation<?> findTestedMethodCall(CtMethod<?> testMethod) {
         // @TODO: Use more sophisticated detection of tested method.
 
         CtInvocation<?> testedMethodCall = null;
 
-        List<CtInvocation<?>> methodCalls = testMethodDeclaration.getElements(new TypeFilter<>(CtInvocation.class));
+        List<CtInvocation<?>> methodCalls = testMethod.getElements(new TypeFilter<>(CtInvocation.class));
         for (CtInvocation<?> methodCall : methodCalls) {
             if (methodCall.getExecutable().getSimpleName().startsWith("assert")) {
                 break;
@@ -27,15 +32,24 @@ public class TestAnalysis {
         return testedMethodCall;
     }
 
-    public static List<CtInvocation<?>> findAssertCalls(CtMethod<?> testMethodDeclaration) {
-        List<CtInvocation<?>> methodCalls = testMethodDeclaration.getElements(new TypeFilter<>(CtInvocation.class));
-        return methodCalls.stream().filter(m -> m.getExecutable().getSimpleName().startsWith("assert")).collect(Collectors.toList());
+    public static List<CtInvocation<?>> findAssertCalls(CtMethod<?> testMethod) {
+        return testMethod.getElements(element -> {
+            CtExecutableReference<?> executable = element.getExecutable();
+            CtTypeReference<?> declaringType = executable.getDeclaringType();
+
+            if (declaringType == null) {
+                return false;
+            }
+
+            String qualifiedName = declaringType.getQualifiedName();
+            return qualifiedName.equals(JUNIT4_ASSERTION_PACKAGE) || qualifiedName.equals(JUNIT5_ASSERTION_PACKAGE);
+        });
     }
 
-    public static CtInvocation<?> findAssertEqualsCall(CtMethod<?> testMethodDeclaration) {
+    public static CtInvocation<?> findAssertEqualsCall(CtMethod<?> testMethod) {
         // @TODO: Use more sophisticated detection of generalizable assertEquals calls.
 
-        List<CtInvocation<?>> methodCalls = testMethodDeclaration.getElements(new TypeFilter<>(CtInvocation.class));
+        List<CtInvocation<?>> methodCalls = testMethod.getElements(new TypeFilter<>(CtInvocation.class));
         List<CtInvocation<?>> assertEqualsCalls = methodCalls.stream().filter(m -> m.getExecutable().getSimpleName().equals("assertEquals")).collect(Collectors.toList());
 
         assert assertEqualsCalls.size() == 1;
