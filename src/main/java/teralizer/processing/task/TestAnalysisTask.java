46c71f441db9f2ce00058f21291917dd2e63b748
Johann Glock <johann.glock@aau.at>
2024-12-29 16:36:40 +0100

    Refactor TestAnalysisTask to base-/sub-task architecture
    This ensures that the TestAnalysisTask can make use of the default
    exclusion handling logic, rather than having to (re-)implement some
    custom exclusion handling logic.

--- src/main/java/teralizer/processing/task/TestAnalysisTask.java
+++ src/main/java/teralizer/processing/task/TestAnalysisTask.java
@@ -2,17 +2,15 @@ package teralizer.processing.task;
 
 import com.google.gson.Gson;
 import org.jooq.DSLContext;
+import org.jooq.Result;
 import org.jooq.generated.Tables;
 import org.jooq.generated.tables.records.AssertionRecord;
 import org.jooq.generated.tables.records.ProjectRecord;
 import org.jooq.generated.tables.records.TestRecord;
 import spoon.Launcher;
-import spoon.processing.AbstractProcessor;
-import spoon.reflect.CtModel;
 import spoon.reflect.code.CtExpression;
 import spoon.reflect.code.CtInvocation;
 import spoon.reflect.cu.SourcePosition;
-import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtParameter;
 import spoon.reflect.declaration.CtType;
@@ -25,120 +23,112 @@ import teralizer.spoon.analysis.TestAnalysis;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 public class TestAnalysisTask extends AbstractTask {
 
     public TestAnalysisTask(ProcessingStage stage, ProjectRecord projectRecord) {
+        this(stage, projectRecord, null);
+    }
+
+    public TestAnalysisTask(ProcessingStage stage, ProjectRecord projectRecord, TestRecord testRecord) {
         this.stage = stage;
         this.projectRecord = projectRecord;
+        this.testRecord = testRecord;
     }
 
     @Override
     protected void executeInternal(TaskContext context, Consumer<String> reportInfo, Consumer<Task> scheduleTask) throws Exception {
+        if (this.testRecord == null) {
+            this.scheduleTasks(context, scheduleTask);
+        } else {
+            this.executeTask(context);
+        }
+    }
+
+    private void scheduleTasks(TaskContext context, Consumer<Task> scheduleTask) {
+        DSLContext create = context.get(TaskContext.DSL_CONTEXT);
+
+        Result<TestRecord> testRecords = create.selectFrom(Tables.TEST)
+            .where(Tables.TEST.PROJECT_ID.eq(this.projectRecord.getId()))
+            .and(Tables.TEST.IS_INCLUDED.eq(true))
+            .fetch();
+
+        for (TestRecord testRecord : testRecords) {
+            scheduleTask.accept(new TestAnalysisTask(this.stage, this.projectRecord, testRecord));
+        }
+    }
+
+    private void executeTask(TaskContext context) {
         DSLContext create = context.get(TaskContext.DSL_CONTEXT);
         Gson gson = context.get(TaskContext.GSON);
 
         Launcher spoonLauncher = context.get(this.getProjectId(), TaskContext.SPOON_LAUNCHER);
 
-        this.analyzeTests(create, gson, spoonLauncher);
-    }
+        CtType<?> testClass = spoonLauncher.getFactory().Type().get(this.testRecord.getTestClassQualifiedName());
+        CtMethod<?> testMethod = testClass.getMethod(this.testRecord.getTestMethodName());
 
-    private void analyzeTests(DSLContext create, Gson gson, Launcher spoonLauncher) {
-        Map<String, List<TestRecord>> allTestRecords = create.selectFrom(Tables.TEST)
-            .where(Tables.TEST.PROJECT_ID.eq(this.getProjectId()))
-            .fetch().stream().collect(Collectors.groupingBy(TestRecord::getTestClassQualifiedName));
-
-        if (allTestRecords.isEmpty()) {
-            throw new RuntimeException("Failed to identify any tests to analyze.");
+        if (testMethod == null) {
+            List<CtMethod<?>> testMethods = testClass.getMethodsByName(this.testRecord.getTestMethodName());
+            if (testMethods.isEmpty()) {
+                // This can happen if the test method was inherited from some other class.
+                // The JUnit reports list the test as part of the child class then, but
+                // the source code file of the child class does not contain the method.
+                throw new RuntimeException("Method " + this.testRecord.getTestMethodName() + " not found in " + this.testRecord.getTestClassQualifiedName() + " (might be inherited).");
+            } else {
+                throw new RuntimeException("Method " + this.testRecord.getTestMethodQualifiedName() + " has parameters.");
+            }
         }
 
-        CtModel ctModel = spoonLauncher.getModel();
-        ctModel.processWith(new AbstractProcessor<CtClass<?>>() {
-            @Override
-            public void process(CtClass<?> ctClass) {
-                if (!allTestRecords.containsKey(ctClass.getQualifiedName())) {
-                    return;
-                }
-                List<TestRecord> testRecords = allTestRecords.get(ctClass.getQualifiedName());
-                for (TestRecord testRecord : testRecords) {
-                    List<CtMethod<?>> testMethods = ctClass.getMethodsByName(testRecord.getTestMethodName());
-                    if (testMethods.isEmpty()) {
-                        // This can happen if the test method was inherited from some other class.
-                        // The JUnit reports list the test as part of the child class then, but
-                        // the source code file of the child class does not contain the method.
-                        testRecord.setIsIncluded(false);
-                        testRecord.setExclusionInfo("Excluded by " + this + ". Method " + TestAnalysisTask.this.testRecord.getTestMethodName() + " not found in " + TestAnalysisTask.this.testRecord.getTestClassQualifiedName() + " (might be inherited).");
-                        testRecord.store();
-                        continue;
-                    }
+        if (testMethod.getAnnotations().stream().noneMatch(a -> a.getType().getSimpleName().equals("Test"))) {
+            throw new RuntimeException("Method " + this.testRecord.getTestMethodQualifiedName() + " has no @Test annotation.");
+        }
 
-                    if (testMethods.size() > 1) {
-                        // This should never happen because there can only be multiple methods
-                        // with the same name if they have different signatures. However, all
-                        // @Test methods should have the same signature (no inputs, void output).
-                        testRecord.setIsIncluded(false);
-                        testRecord.setExclusionInfo("Excluded by " + this + ". Multiple methods with name " + TestAnalysisTask.this.testRecord.getTestMethodName() + " found in " + TestAnalysisTask.this.testRecord.getTestClassQualifiedName() + ".");
-                        testRecord.store();
-                        continue;
-                    }
+        TestAnalysisTask.createAssertionRecords(this.testRecord, testMethod, create, gson);
 
-                    CtMethod<?> testMethod = testMethods.get(0);
-                    if (testMethod.getAnnotations().stream().noneMatch(a -> a.getType().getSimpleName().equals("Test"))) {
-                        testRecord.setIsIncluded(false);
-                        testRecord.setExclusionInfo("Excluded by " + this + ". Test method has no @Test annotation.");
-                        testRecord.store();
-                        continue;
-                    }
+        CtInvocation<?> assertion = TestAnalysis.findGeneralizableAssert(testMethod).orElse(null);
+        CtInvocation<?> testedMethodCall = TestAnalysis.findTestedMethodCall(testMethod, assertion).orElse(null);
 
-                    TestAnalysisTask.createAssertionRecords(testRecord, testMethod, create, gson);
+        if (testedMethodCall == null) {
+            // Unable to identify tested method.
+            return;
+        }
 
-                    CtInvocation<?> assertion = TestAnalysis.findGeneralizableAssert(testMethod).orElse(null);
-                    CtInvocation<?> testedMethodCall = TestAnalysis.findTestedMethodCall(testMethod, assertion).orElse(null);
+        CtMethod<?> testedMethod = (CtMethod<?>) testedMethodCall.getExecutable().getDeclaration();
+        CtType<?> testedType = testedMethod == null ? null : testedMethod.getDeclaringType();
 
-                    if (testedMethodCall == null) {
-                        continue;
-                    }
+        if (testedMethod == null || testedType == null) {
+            // Unable to identify tested method.
+            return;
+        }
 
-                    CtMethod<?> testedMethod = (CtMethod<?>) testedMethodCall.getExecutable().getDeclaration();
-                    CtType<?> testedType = testedMethod == null ? null : testedMethod.getDeclaringType();
+        SourcePosition position = testedType.getPosition();
+        String testedClassPath = Paths.get(System.getProperty("user.dir")).relativize(position.getFile().toPath()).toString();
 
-                    if (testedMethod == null || testedType == null) {
-                        continue;
-                    }
+        String packageName = testedType.getPackage().toString();
+        String className = testedType.getSimpleName();
+        String methodName = testedMethod.getSimpleName();
 
-                    SourcePosition position = testedType.getPosition();
-                    String testedClassPath = Paths.get(System.getProperty("user.dir")).relativize(position.getFile().toPath()).toString();
+        String qualifiedClassName = testedType.getQualifiedName();
+        String qualifiedMethodName = qualifiedClassName + "." + methodName;
 
-                    String packageName = testedType.getPackage().toString();
-                    String className = testedType.getSimpleName();
-                    String methodName = testedMethod.getSimpleName();
+        List<MethodParameter> testedMethodParameters = new ArrayList<>();
+        for (CtParameter<?> param : testedMethod.getParameters()) {
+            String paramType = param.getType().getQualifiedName();
+            String paramName = param.getSimpleName();
+            testedMethodParameters.add(new MethodParameter(paramType, paramName));
+        }
 
-                    String qualifiedClassName = testedType.getQualifiedName();
-                    String qualifiedMethodName = qualifiedClassName + "." + methodName;
+        this.testRecord.setTestedFilePath(testedClassPath);
+        this.testRecord.setTestedClassQualifiedName(qualifiedClassName);
+        this.testRecord.setTestedMethodQualifiedName(qualifiedMethodName);
+        this.testRecord.setTestedPackageName(packageName);
+        this.testRecord.setTestedClassName(className);
+        this.testRecord.setTestedMethodName(methodName);
+        this.testRecord.setTestedMethodParamTypes(gson.toJson(testedMethodParameters));
+        this.testRecord.setTestedMethodReturnType(testedMethod.getType().getQualifiedName());
 
-                    List<MethodParameter> testedMethodParameters = new ArrayList<>();
-                    for (CtParameter<?> param : testedMethod.getParameters()) {
-                        String paramType = param.getType().getQualifiedName();
-                        String paramName = param.getSimpleName();
-                        testedMethodParameters.add(new MethodParameter(paramType, paramName));
-                    }
-
-                    testRecord.setTestedFilePath(testedClassPath);
-                    testRecord.setTestedClassQualifiedName(qualifiedClassName);
-                    testRecord.setTestedMethodQualifiedName(qualifiedMethodName);
-                    testRecord.setTestedPackageName(packageName);
-                    testRecord.setTestedClassName(className);
-                    testRecord.setTestedMethodName(methodName);
-                    testRecord.setTestedMethodParamTypes(gson.toJson(testedMethodParameters));
-                    testRecord.setTestedMethodReturnType(testedMethod.getType().getQualifiedName());
-
-                    testRecord.store();
-                }
-            }
-        });
+        this.testRecord.store();
     }
 
     public static void createAssertionRecords(TestRecord testRecord, CtMethod<?> testMethod, DSLContext create, Gson gson) {
