glockyco/Teralizer

commit 6f7ab3e2dc37ba298f6992e1657b3dabf39a521e

Create generalizations for assertions instead of tests

src/main/java/teralizer/processing/filter/AssertionCountFilter.java
@@ -25,8 +25,6 @@ public class AssertionCountFilter extends AbstractFilter {
 
         if (assertions.isEmpty()) {
             return new FilterResult(this.getName(), FilterDecision.REJECT, "assertions.size() < 1.");
-        } else if (assertions.size() > 1) {
-            return new FilterResult(this.getName(), FilterDecision.REJECT, "assertions.size() > 1.");
         }
 
         return new FilterResult(this.getName(), FilterDecision.ACCEPT);

src/main/java/teralizer/processing/task/TestAnalysisTask.java
@@ -14,7 +14,7 @@ import spoon.reflect.cu.SourcePosition;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtParameter;
 import spoon.reflect.declaration.CtType;
-import spoon.reflect.reference.CtExecutableReference;
+import teralizer.domain.MethodArgument;
 import teralizer.domain.MethodParameter;
 import teralizer.processing.ProcessingStage;
 import teralizer.processing.TaskContext;
@@ -49,9 +49,9 @@ public class TestAnalysisTask extends AbstractTask {
     private void scheduleTasks(TaskContext context, Consumer<Task> scheduleTask) {
         DSLContext create = context.get(TaskContext.DSL_CONTEXT);
 
+        // Analysis is intentionally performed for ALL tests, not just included ones.
         Result<TestRecord> testRecords = create.selectFrom(Tables.TEST)
             .where(Tables.TEST.PROJECT_ID.eq(this.projectRecord.getId()))
-            .and(Tables.TEST.IS_INCLUDED.eq(true))
             .fetch();
 
         for (TestRecord testRecord : testRecords) {
@@ -84,78 +84,84 @@ public class TestAnalysisTask extends AbstractTask {
             throw new RuntimeException("Method " + this.testRecord.getTestMethodQualifiedName() + " has no @Test annotation.");
         }
 
-        TestAnalysisTask.createAssertionRecords(this.testRecord, testMethod, create, gson);
-
-        CtInvocation<?> assertion = TestAnalysis.findGeneralizableAssert(testMethod).orElse(null);
-        CtInvocation<?> testedMethodCall = TestAnalysis.findTestedMethodCall(testMethod, assertion).orElse(null);
-
-        if (testedMethodCall == null) {
-            // Unable to identify tested method.
-            return;
-        }
-
-        CtMethod<?> testedMethod = (CtMethod<?>) testedMethodCall.getExecutable().getDeclaration();
-        CtType<?> testedType = testedMethod == null ? null : testedMethod.getDeclaringType();
-
-        if (testedMethod == null || testedType == null) {
-            // Unable to identify tested method.
-            return;
-        }
-
-        SourcePosition position = testedType.getPosition();
-        String testedClassPath = Paths.get(System.getProperty("user.dir")).relativize(position.getFile().toPath()).toString();
-
-        String packageName = testedType.getPackage().toString();
-        String className = testedType.getSimpleName();
-        String methodName = testedMethod.getSimpleName();
-
-        String qualifiedClassName = testedType.getQualifiedName();
-        String qualifiedMethodName = qualifiedClassName + "." + methodName;
-
-        List<MethodParameter> testedMethodParameters = new ArrayList<>();
-        for (CtParameter<?> param : testedMethod.getParameters()) {
-            String paramType = param.getType().getQualifiedName();
-            String paramName = param.getSimpleName();
-            testedMethodParameters.add(new MethodParameter(paramType, paramName));
-        }
-
-        this.testRecord.setTestedFilePath(testedClassPath);
-        this.testRecord.setTestedClassQualifiedName(qualifiedClassName);
-        this.testRecord.setTestedMethodQualifiedName(qualifiedMethodName);
-        this.testRecord.setTestedPackageName(packageName);
-        this.testRecord.setTestedClassName(className);
-        this.testRecord.setTestedMethodName(methodName);
-        this.testRecord.setTestedMethodParamTypes(gson.toJson(testedMethodParameters));
-        this.testRecord.setTestedMethodReturnType(testedMethod.getType().getQualifiedName());
-
-        this.testRecord.store();
+        this.createAssertionRecords(testMethod, create, gson);
     }
 
-    public static void createAssertionRecords(TestRecord testRecord, CtMethod<?> testMethod, DSLContext create, Gson gson) {
-        List<AssertionRecord> assertionRecords = new ArrayList<>();
+    private void createAssertionRecords(CtMethod<?> testMethod, DSLContext create, Gson gson) {
+        List<AssertionRecord> records = new ArrayList<>();
 
-        List<CtInvocation<?>> assertMethodCalls = TestAnalysis.findAllAsserts(testMethod);
-        for (CtInvocation<?> assertMethodCall : assertMethodCalls) {
-            CtExecutableReference<?> assertMethodRef = assertMethodCall.getExecutable();
-            String methodName = assertMethodRef.getSimpleName();
-            String sourceCode = assertMethodCall.toString();
-
-            List<MethodParameter> assertArguments = new ArrayList<>();
-            for (CtExpression<?> argument : assertMethodCall.getArguments()) {
-                String paramType = argument.getType().getQualifiedName();
-                String paramValue = argument.toString();
-                assertArguments.add(new MethodParameter(paramType, paramValue));
+        List<CtInvocation<?>> assertionCalls = TestAnalysis.findAllAsserts(testMethod);
+        for (CtInvocation<?> assertionCall : assertionCalls) {
+            List<MethodArgument> assertionArguments = new ArrayList<>();
+            for (CtExpression<?> argument : assertionCall.getArguments()) {
+                String argType = argument.getType().getQualifiedName();
+                String argValue = argument.toString();
+                assertionArguments.add(new MethodArgument(argType, argValue));
             }
 
-            AssertionRecord assertionRecord = create.newRecord(Tables.ASSERTION);
-            assertionRecord.setProjectId(testRecord.getProjectId());
-            assertionRecord.setTestId(testRecord.getId());
-            assertionRecord.setMethodName(methodName);
-            assertionRecord.setMethodArgumentTypes(gson.toJson(assertArguments));
-            assertionRecord.setSourceCode(sourceCode);
-            assertionRecords.add(assertionRecord);
+            AssertionRecord record = create.newRecord(Tables.ASSERTION);
+            record.setProjectId(this.getProjectId());
+            record.setTestId(this.getTestId());
+
+            record.setAssertionName(assertionCall.getExecutable().getSimpleName());
+            record.setAssertionArguments(gson.toJson(assertionArguments));
+            record.setAssertionSourceCode(assertionCall.toString());
+            record.setAssertionAbsolutePath(assertionCall.getPath().toString());
+            record.setAssertionRelativePath(assertionCall.getPath().relativePath(testMethod).toString());
+
+            CtInvocation<?> testedMethodCall = TestAnalysis.findTestedMethodCall(testMethod, assertionCall).orElse(null);
+
+            if (testedMethodCall != null) {
+                List<MethodArgument> methodArguments = new ArrayList<>();
+                for (CtExpression<?> argument : testedMethodCall.getArguments()) {
+                    String argType = argument.getType().getQualifiedName();
+                    String argValue = argument.toString();
+                    methodArguments.add(new MethodArgument(argType, argValue));
+                }
+
+                record.setTestedMethodName(testedMethodCall.getExecutable().getSimpleName());
+                record.setTestedMethodCallArguments(gson.toJson(methodArguments));
+                record.setTestedMethodCallSourceCode(testedMethodCall.toString());
+                record.setTestedMethodCallAbsolutePath(testedMethodCall.getPath().toString());
+                record.setTestedMethodCallRelativePath(testedMethodCall.getPath().relativePath(testMethod).toString());
+
+                CtMethod<?> testedMethod = (CtMethod<?>) testedMethodCall.getExecutable().getDeclaration();
+                CtType<?> testedType = testedMethod == null ? null : testedMethod.getDeclaringType();
+
+                if (testedMethod != null && testedType != null) {
+                    SourcePosition position = testedType.getPosition();
+                    String testedClassPath = Paths.get(System.getProperty("user.dir")).relativize(position.getFile().toPath()).toString();
+
+                    String packageName = testedType.getPackage().toString();
+                    String className = testedType.getSimpleName();
+                    String methodName = testedMethod.getSimpleName();
+
+                    String qualifiedClassName = testedType.getQualifiedName();
+                    String qualifiedMethodName = qualifiedClassName + "." + methodName;
+
+                    List<MethodParameter> testedMethodParameters = new ArrayList<>();
+                    for (CtParameter<?> param : testedMethod.getParameters()) {
+                        String paramType = param.getType().getQualifiedName();
+                        String paramName = param.getSimpleName();
+                        testedMethodParameters.add(new MethodParameter(paramType, paramName));
+                    }
+
+                    record.setTestedFilePath(testedClassPath);
+                    record.setTestedClassQualifiedName(qualifiedClassName);
+                    record.setTestedMethodQualifiedName(qualifiedMethodName);
+                    record.setTestedPackageName(packageName);
+                    record.setTestedClassName(className);
+                    record.setTestedMethodName(methodName);
+                    record.setTestedMethodParameters(gson.toJson(testedMethodParameters));
+                    record.setTestedMethodReturnType(testedMethod.getType().getQualifiedName());
+                }
+            }
+
+            record.setIsIncluded(true);
+
+            records.add(record);
         }
 
-        create.batchStore(assertionRecords).execute();
+        create.batchStore(records).execute();
     }
 }

src/main/java/teralizer/processing/task/TestFilteringTask.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import org.jooq.DSLContext;
 import org.jooq.Record;
 import org.jooq.Result;
+import org.jooq.generated.tables.records.AssertionRecord;
 import org.jooq.generated.tables.records.GeneralizationRecord;
 import org.jooq.generated.tables.records.ProjectRecord;
 import org.jooq.generated.tables.records.TestRecord;
@@ -11,10 +12,6 @@ import teralizer.processing.GeneralizationVariant;
 import teralizer.processing.ProcessingStage;
 import teralizer.processing.TaskContext;
 import teralizer.processing.filter.*;
-import teralizer.processing.filter.AssertionCountFilter;
-import teralizer.processing.filter.MissingValueFilter;
-import teralizer.processing.filter.ParameterTypeFilter;
-import teralizer.processing.filter.Filter;
 import teralizer.repository.SQLiteRepository;
 
 import java.util.ArrayList;
@@ -27,15 +24,15 @@ import java.util.stream.Collectors;
 public class TestFilteringTask extends AbstractTask {
 
     public TestFilteringTask(ProcessingStage stage, ProjectRecord projectRecord) {
-        this(stage, projectRecord, null);
+        this(stage, projectRecord, null, null);
     }
 
-    public TestFilteringTask(ProcessingStage stage, ProjectRecord projectRecord, TestRecord testRecord) {
-        this(stage, null, projectRecord, testRecord, null);
+    public TestFilteringTask(ProcessingStage stage, ProjectRecord projectRecord, TestRecord testRecord, AssertionRecord assertionRecord) {
+        this(stage, null, projectRecord, testRecord, assertionRecord, null);
     }
 
     public TestFilteringTask(ProcessingStage stage, GeneralizationVariant variant, ProjectRecord projectRecord) {
-        this(stage, variant, projectRecord, null, null);
+        this(stage, variant, projectRecord, null, null, null);
     }
 
     public TestFilteringTask(
@@ -43,12 +40,14 @@ public class TestFilteringTask extends AbstractTask {
         GeneralizationVariant variant,
         ProjectRecord projectRecord,
         TestRecord testRecord,
+        AssertionRecord assertionRecord,
         GeneralizationRecord generalizationRecord
     ) {
         this.stage = stage;
         this.variant = variant;
         this.projectRecord = projectRecord;
         this.testRecord = testRecord;
+        this.assertionRecord = assertionRecord;
         this.generalizationRecord = generalizationRecord;
     }
 
@@ -74,16 +73,19 @@ public class TestFilteringTask extends AbstractTask {
     private void scheduleTasks(TaskContext context, Consumer<Task> scheduleTask) {
         DSLContext create = context.get(TaskContext.DSL_CONTEXT);
         if (this.stage == ProcessingStage.FILTER_TESTS) {
-            Result<TestRecord> testRecords = SQLiteRepository.fetchIncludedTests(create, this.getProjectId());
-            for (TestRecord testRecord : testRecords) {
-                scheduleTask.accept(new TestFilteringTask(this.stage, this.projectRecord, testRecord));
+            Result<Record> records = SQLiteRepository.fetchIncludedAssertions(create, this.getProjectId());
+            for (Record record : records) {
+                TestRecord testRecord = record.into(TestRecord.class);
+                AssertionRecord assertionRecord = record.into(AssertionRecord.class);
+                scheduleTask.accept(new TestFilteringTask(this.stage, this.projectRecord, testRecord, assertionRecord));
             }
         } else if (this.stage == ProcessingStage.FILTER_GENERALIZATIONS) {
             Result<Record> records = SQLiteRepository.fetchIncludedGeneralizations(create, this.variant, this.getProjectId());
             for (Record record : records) {
                 TestRecord testRecord = record.into(TestRecord.class);
+                AssertionRecord assertionRecord = record.into(AssertionRecord.class);
                 GeneralizationRecord generalizationRecord = record.into(GeneralizationRecord.class);
-                scheduleTask.accept(new TestFilteringTask(this.stage, this.variant, this.projectRecord, testRecord, generalizationRecord));
+                scheduleTask.accept(new TestFilteringTask(this.stage, this.variant, this.projectRecord, testRecord, assertionRecord, generalizationRecord));
             }
         } else {
             throw new RuntimeException("Unsupported processing stage " + this.stage + ".");
@@ -96,9 +98,9 @@ public class TestFilteringTask extends AbstractTask {
 
         List<Filter> filters = Arrays.asList(
             new AssertionCountFilter(create, this.testRecord),
-            new MissingValueFilter(this.testRecord),
+            new MissingValueFilter(this.assertionRecord),
             new UnnamedPackageFilter(this.testRecord),
-            new ParameterTypeFilter(gson, this.testRecord),
+            new ParameterTypeFilter(gson, this.assertionRecord),
             new NonPassingTestFilter(create, this.testRecord)
         );
 
@@ -131,6 +133,10 @@ public class TestFilteringTask extends AbstractTask {
                 this.generalizationRecord.setIsIncluded(false);
                 this.generalizationRecord.setExclusionInfo("Excluded by " + this + ".\n\n" + info);
                 this.generalizationRecord.store();
+            } else if (this.assertionRecord != null) {
+                this.assertionRecord.setIsIncluded(false);
+                this.assertionRecord.setExclusionInfo("Excluded by " + this + ".\n\n" + info);
+                this.assertionRecord.store();
             } else if (this.testRecord != null) {
                 this.testRecord.setIsIncluded(false);
                 this.testRecord.setExclusionInfo("Excluded by " + this + ".\n\n" + info);

build.gradle
@@ -109,6 +109,11 @@ jooq {
                                 types = "INTEGER"
                             }
                             forcedType {
+                                name = "BOOLEAN"
+                                expressions = "assertion.is_included"
+                                types = "INTEGER"
+                            }
+                            forcedType {
                                 userType = "teralizer.processing.GeneralizationVariant"
                                 enumConverter = true
                                 includeExpression = "generalization.variant"

build/generated-src/jooq/main/org/jooq/generated/Indexes.java
@@ -29,8 +29,10 @@ public class Indexes {
     // INDEX definitions
     // -------------------------------------------------------------------------
 
+    public static final Index IDX_ASSERTION_IS_INCLUDED = Internal.createIndex(DSL.name("idx_assertion_is_included"), Assertion.ASSERTION, new OrderField[] { Assertion.ASSERTION.IS_INCLUDED }, false);
     public static final Index IDX_ASSERTION_PROJECT_ID = Internal.createIndex(DSL.name("idx_assertion_project_id"), Assertion.ASSERTION, new OrderField[] { Assertion.ASSERTION.PROJECT_ID }, false);
     public static final Index IDX_ASSERTION_TEST_ID = Internal.createIndex(DSL.name("idx_assertion_test_id"), Assertion.ASSERTION, new OrderField[] { Assertion.ASSERTION.TEST_ID }, false);
+    public static final Index IDX_GENERALIZATION_ASSERTION_ID = Internal.createIndex(DSL.name("idx_generalization_assertion_id"), Generalization.GENERALIZATION, new OrderField[] { Generalization.GENERALIZATION.ASSERTION_ID }, false);
     public static final Index IDX_GENERALIZATION_CLASS_QUALIFIED_NAME = Internal.createIndex(DSL.name("idx_generalization_class_qualified_name"), Generalization.GENERALIZATION, new OrderField[] { Generalization.GENERALIZATION.CLASS_QUALIFIED_NAME }, false);
     public static final Index IDX_GENERALIZATION_FILE_PATH = Internal.createIndex(DSL.name("idx_generalization_file_path"), Generalization.GENERALIZATION, new OrderField[] { Generalization.GENERALIZATION.FILE_PATH }, false);
     public static final Index IDX_GENERALIZATION_IS_INCLUDED = Internal.createIndex(DSL.name("idx_generalization_is_included"), Generalization.GENERALIZATION, new OrderField[] { Generalization.GENERALIZATION.IS_INCLUDED }, false);
@@ -67,6 +69,7 @@ public class Indexes {
     public static final Index IDX_PROJECT_PATH = Internal.createIndex(DSL.name("idx_project_path"), Project.PROJECT, new OrderField[] { Project.PROJECT.ROOT_PATH }, false);
     public static final Index IDX_PROJECT_TEST_FRAMEWORK = Internal.createIndex(DSL.name("idx_project_test_framework"), Project.PROJECT, new OrderField[] { Project.PROJECT.TEST_FRAMEWORK }, false);
     public static final Index IDX_PROJECT_TYPE = Internal.createIndex(DSL.name("idx_project_type"), Project.PROJECT, new OrderField[] { Project.PROJECT.TYPE }, false);
+    public static final Index IDX_TASK_ASSERTION_ID = Internal.createIndex(DSL.name("idx_task_assertion_id"), Task.TASK, new OrderField[] { Task.TASK.ASSERTION_ID }, false);
     public static final Index IDX_TASK_GENERALIZATION_ID = Internal.createIndex(DSL.name("idx_task_generalization_id"), Task.TASK, new OrderField[] { Task.TASK.GENERALIZATION_ID }, false);
     public static final Index IDX_TASK_PROJECT_ID = Internal.createIndex(DSL.name("idx_task_project_id"), Task.TASK, new OrderField[] { Task.TASK.PROJECT_ID }, false);
     public static final Index IDX_TASK_STAGE = Internal.createIndex(DSL.name("idx_task_stage"), Task.TASK, new OrderField[] { Task.TASK.STAGE }, false);
@@ -79,7 +82,7 @@ public class Indexes {
     public static final Index IDX_TEST_TEST_CLASS_NAME = Internal.createIndex(DSL.name("idx_test_test_class_name"), Test.TEST, new OrderField[] { Test.TEST.TEST_CLASS_NAME }, false);
     public static final Index IDX_TEST_TEST_CLASS_QUALIFIED_NAME = Internal.createIndex(DSL.name("idx_test_test_class_qualified_name"), Test.TEST, new OrderField[] { Test.TEST.TEST_CLASS_QUALIFIED_NAME }, false);
     public static final Index IDX_TEST_TEST_FILE_PATH = Internal.createIndex(DSL.name("idx_test_test_file_path"), Test.TEST, new OrderField[] { Test.TEST.TEST_FILE_PATH }, false);
-    public static final Index IDX_TEST_TEST_METHOD_NAME = Internal.createIndex(DSL.name("idx_test_test_method_name"), Test.TEST, new OrderField[] { Test.TEST.TESTED_METHOD_NAME }, false);
+    public static final Index IDX_TEST_TEST_METHOD_NAME = Internal.createIndex(DSL.name("idx_test_test_method_name"), Test.TEST, new OrderField[] { Test.TEST.TEST_METHOD_NAME }, false);
     public static final Index IDX_TEST_TEST_METHOD_QUALIFIED_NAME = Internal.createIndex(DSL.name("idx_test_test_method_qualified_name"), Test.TEST, new OrderField[] { Test.TEST.TEST_METHOD_QUALIFIED_NAME }, false);
     public static final Index IDX_TEST_TEST_PACKAGE_NAME = Internal.createIndex(DSL.name("idx_test_test_package_name"), Test.TEST, new OrderField[] { Test.TEST.TEST_PACKAGE_NAME }, false);
 }

build/generated-src/jooq/main/org/jooq/generated/Keys.java
@@ -55,6 +55,8 @@ public class Keys {
     // -------------------------------------------------------------------------
 
     public static final ForeignKey<AssertionRecord, TestRecord> FK_ASSERTION_TEST_1 = Internal.createForeignKey(Assertion.ASSERTION, DSL.name("fk_assertion_test_1"), new TableField[] { Assertion.ASSERTION.TEST_ID }, Keys.PK_TEST, new TableField[] { Test.TEST.ID }, true);
+    public static final ForeignKey<GeneralizationRecord, AssertionRecord> FK_GENERALIZATION_ASSERTION_1 = Internal.createForeignKey(Generalization.GENERALIZATION, DSL.name("fk_generalization_assertion_1"), new TableField[] { Generalization.GENERALIZATION.ASSERTION_ID }, Keys.PK_ASSERTION, new TableField[] { Assertion.ASSERTION.ID }, true);
+    public static final ForeignKey<GeneralizationRecord, ProjectRecord> FK_GENERALIZATION_PROJECT_1 = Internal.createForeignKey(Generalization.GENERALIZATION, DSL.name("fk_generalization_project_1"), new TableField[] { Generalization.GENERALIZATION.PROJECT_ID }, Keys.PK_PROJECT, new TableField[] { Project.PROJECT.ID }, true);
     public static final ForeignKey<GeneralizationRecord, TestRecord> FK_GENERALIZATION_TEST_1 = Internal.createForeignKey(Generalization.GENERALIZATION, DSL.name("fk_generalization_test_1"), new TableField[] { Generalization.GENERALIZATION.TEST_ID }, Keys.PK_TEST, new TableField[] { Test.TEST.ID }, true);
     public static final ForeignKey<JacocoCoverageReportRecord, ProjectRecord> FK_JACOCO_COVERAGE_REPORT_PROJECT_1 = Internal.createForeignKey(JacocoCoverageReport.JACOCO_COVERAGE_REPORT, DSL.name("fk_jacoco_coverage_report_project_1"), new TableField[] { JacocoCoverageReport.JACOCO_COVERAGE_REPORT.PROJECT_ID }, Keys.PK_PROJECT, new TableField[] { Project.PROJECT.ID }, true);
     public static final ForeignKey<JunitTestReportRecord, GeneralizationRecord> FK_JUNIT_TEST_REPORT_GENERALIZATION_1 = Internal.createForeignKey(JunitTestReport.JUNIT_TEST_REPORT, DSL.name("fk_junit_test_report_generalization_1"), new TableField[] { JunitTestReport.JUNIT_TEST_REPORT.GENERALIZATION_ID }, Keys.PK_GENERALIZATION, new TableField[] { Generalization.GENERALIZATION.ID }, true);
@@ -64,6 +66,7 @@ public class Keys {
     public static final ForeignKey<PitCoverageReportRecord, ProjectRecord> FK_PIT_COVERAGE_REPORT_PROJECT_1 = Internal.createForeignKey(PitCoverageReport.PIT_COVERAGE_REPORT, DSL.name("fk_pit_coverage_report_project_1"), new TableField[] { PitCoverageReport.PIT_COVERAGE_REPORT.PROJECT_ID }, Keys.PK_PROJECT, new TableField[] { Project.PROJECT.ID }, true);
     public static final ForeignKey<PitCoverageReportRecord, TestRecord> FK_PIT_COVERAGE_REPORT_TEST_1 = Internal.createForeignKey(PitCoverageReport.PIT_COVERAGE_REPORT, DSL.name("fk_pit_coverage_report_test_1"), new TableField[] { PitCoverageReport.PIT_COVERAGE_REPORT.TEST_ID }, Keys.PK_TEST, new TableField[] { Test.TEST.ID }, true);
     public static final ForeignKey<PitMutationReportRecord, ProjectRecord> FK_PIT_MUTATION_REPORT_PROJECT_1 = Internal.createForeignKey(PitMutationReport.PIT_MUTATION_REPORT, DSL.name("fk_pit_mutation_report_project_1"), new TableField[] { PitMutationReport.PIT_MUTATION_REPORT.PROJECT_ID }, Keys.PK_PROJECT, new TableField[] { Project.PROJECT.ID }, true);
+    public static final ForeignKey<TaskRecord, AssertionRecord> FK_TASK_ASSERTION_1 = Internal.createForeignKey(Task.TASK, DSL.name("fk_task_assertion_1"), new TableField[] { Task.TASK.ASSERTION_ID }, Keys.PK_ASSERTION, new TableField[] { Assertion.ASSERTION.ID }, true);
     public static final ForeignKey<TaskRecord, GeneralizationRecord> FK_TASK_GENERALIZATION_1 = Internal.createForeignKey(Task.TASK, DSL.name("fk_task_generalization_1"), new TableField[] { Task.TASK.GENERALIZATION_ID }, Keys.PK_GENERALIZATION, new TableField[] { Generalization.GENERALIZATION.ID }, true);
     public static final ForeignKey<TaskRecord, ProjectRecord> FK_TASK_PROJECT_1 = Internal.createForeignKey(Task.TASK, DSL.name("fk_task_project_1"), new TableField[] { Task.TASK.PROJECT_ID }, Keys.PK_PROJECT, new TableField[] { Project.PROJECT.ID }, true);
     public static final ForeignKey<TaskRecord, TestRecord> FK_TASK_TEST_1 = Internal.createForeignKey(Task.TASK, DSL.name("fk_task_test_1"), new TableField[] { Task.TASK.TEST_ID }, Keys.PK_TEST, new TableField[] { Test.TEST.ID }, true);

commit bd679911394d9eebaca8e559176928aa4a83692d

Fix marking of tests without assertions as excluded
Until now, tests without assertions were (supposed to be) marked as
excluded by the AssertionCountFilter / TestAnalysisTask.

This broke with 6f7ab3e2dc37ba298f6992e1657b3dabf39a521e because since
that commit, the TestFilterTask is only applied to tests that contain
at least one assertion (we are actually filtering assertions rather
than tests, because assertions are now the generalization target).

To restore this functionality, the TestAnalysisTask now throws an
exception if any of the processed tests do not contain any assertions.

src/main/java/teralizer/processing/filter/AssertionCountFilter.java
@@ -1,32 +1,0 @@
-package teralizer.processing.filter;
-
-import org.jooq.DSLContext;
-import org.jooq.generated.Tables;
-import org.jooq.generated.tables.records.AssertionRecord;
-import org.jooq.generated.tables.records.TestRecord;
-
-import java.util.List;
-
-public class AssertionCountFilter extends AbstractFilter {
-
-    private final DSLContext create;
-    private final TestRecord testRecord;
-
-    public AssertionCountFilter(DSLContext create, TestRecord testRecord) {
-        this.create = create;
-        this.testRecord = testRecord;
-    }
-
-    @Override
-    public FilterResult check() {
-        List<AssertionRecord> assertions = this.create.selectFrom(Tables.ASSERTION)
-            .where(Tables.ASSERTION.TEST_ID.equal(this.testRecord.getId()))
-            .fetch();
-
-        if (assertions.isEmpty()) {
-            return new FilterResult(this.getName(), FilterDecision.REJECT, "assertions.size() < 1.");
-        }
-
-        return new FilterResult(this.getName(), FilterDecision.ACCEPT);
-    }
-}

src/main/java/teralizer/processing/task/TestAnalysisTask.java
@@ -91,6 +91,11 @@ public class TestAnalysisTask extends AbstractTask {
         List<AssertionRecord> records = new ArrayList<>();
 
         List<CtInvocation<?>> assertionCalls = TestAnalysis.findAllAsserts(testMethod);
+
+        if (assertionCalls.isEmpty()) {
+            throw new RuntimeException("No assertions found in " + this.testRecord.getTestMethodQualifiedName() + ".");
+        }
+
         for (CtInvocation<?> assertionCall : assertionCalls) {
             List<MethodArgument> assertionArguments = new ArrayList<>();
             for (CtExpression<?> argument : assertionCall.getArguments()) {

src/main/java/teralizer/processing/task/TestFilteringTask.java
@@ -97,7 +97,6 @@ public class TestFilteringTask extends AbstractTask {
         Gson gson = context.get(TaskContext.GSON);
 
         List<Filter> filters = Arrays.asList(
-            new AssertionCountFilter(create, this.testRecord),
             new MissingValueFilter(this.assertionRecord),
             new UnnamedPackageFilter(this.testRecord),
             new ParameterTypeFilter(gson, this.assertionRecord),