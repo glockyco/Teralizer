59a832cafa36295f1bc286f7c33a7ea04e973c1e
Johann Glock <johann.glock@aau.at>
2025-04-06 09:33:12 +0200

    Remove JaCoCo + PIT data collection from test generation
    Generated tests sometimes require manual post-processing to
    remove, e.g., very long-running tests or tests that cause out-of-
    memory exceptions. Thus, it is not useful to run JaCoCo + PIT data
    collection automatically right after test generation.

--- src/main/java/teralizer/processing/task/ProjectSetupTask.java
+++ src/main/java/teralizer/processing/task/ProjectSetupTask.java
@@ -91,15 +91,15 @@ public class ProjectSetupTask extends AbstractTask {
             scheduleTask.accept(new EvoSuitePostprocessingTask(ProcessingStage.POSTPROCESS_EVOSUITE_TESTS, this.projectRecord));
         }
 
-        scheduleTask.accept(new SpoonModelBuildingTask(ProcessingStage.BUILD_SPOON_MODEL, this.projectRecord));
-
-        scheduleTask.accept(new TestExecutionTask(ProcessingStage.EXECUTE_TESTS_ORIGINAL, this.projectRecord));
-        scheduleTask.accept(new JunitDataCollectionTask(ProcessingStage.COLLECT_JUNIT_REPORTS_ORIGINAL, this.projectRecord));
-        scheduleTask.accept(new JacocoDataCollectionTask(ProcessingStage.COLLECT_JACOCO_DATA_ORIGINAL, this.projectRecord));
-        scheduleTask.accept(new TestFilteringTask(ProcessingStage.FILTER_TESTS_ORIGINAL, this.projectRecord));
-        scheduleTask.accept(new PitDataCollectionTask(ProcessingStage.COLLECT_PIT_DATA_ORIGINAL, this.projectRecord));
-
         if (this.projectRecord.getUseTestGeneralization()) {
+            scheduleTask.accept(new SpoonModelBuildingTask(ProcessingStage.BUILD_SPOON_MODEL, this.projectRecord));
+
+            scheduleTask.accept(new TestExecutionTask(ProcessingStage.EXECUTE_TESTS_ORIGINAL, this.projectRecord));
+            scheduleTask.accept(new JunitDataCollectionTask(ProcessingStage.COLLECT_JUNIT_REPORTS_ORIGINAL, this.projectRecord));
+            scheduleTask.accept(new JacocoDataCollectionTask(ProcessingStage.COLLECT_JACOCO_DATA_ORIGINAL, this.projectRecord));
+            scheduleTask.accept(new TestFilteringTask(ProcessingStage.FILTER_TESTS_ORIGINAL, this.projectRecord));
+            scheduleTask.accept(new PitDataCollectionTask(ProcessingStage.COLLECT_PIT_DATA_ORIGINAL, this.projectRecord));
+
             scheduleTask.accept(new TestAnalysisTask(ProcessingStage.ANALYZE_TESTS, this.projectRecord));
             scheduleTask.accept(new TestFilteringTask(ProcessingStage.FILTER_TESTS, this.projectRecord));
             scheduleTask.accept(new TestFilteringTask(ProcessingStage.FILTER_ASSERTIONS, this.projectRecord));
